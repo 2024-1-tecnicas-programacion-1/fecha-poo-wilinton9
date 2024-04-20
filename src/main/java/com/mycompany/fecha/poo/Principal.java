24d2d93a1e90f8779e219a165502c5bb33bde817
wilinton9 <159450242+wilinton9@users.noreply.github.com>
2024-04-19 23:27:09 -0500

    Update Principal.java

--- src/main/java/com/mycompany/fecha/poo/Principal.java
+++ src/main/java/com/mycompany/fecha/poo/Principal.java
@@ -2,4 +2,29 @@ package com.mycompany.fecha.poo;
 
 public class Principal {
     // TODO: Aquí va el código de la clase Principal
+    import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese el día:");
+        int dia = scanner.nextInt();
+        System.out.println("Ingrese el mes:");
+        int mes = scanner.nextInt();
+        System.out.println("Ingrese el año:");
+        int anno = scanner.nextInt();
+
+        Fecha fecha = new Fecha(dia, mes, anno);
+        if (fecha.validarFecha()) {
+            System.out.println("La fecha ingresada es válida:");
+            System.out.println("Fecha larga: " + fecha.fechaLarga());
+        } else {
+            System.out.println("La fecha ingresada no es válida.");
+        }
+
+        scanner.close();
+    }
+}
+
 }
